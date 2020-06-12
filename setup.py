432b20537e57c004b97b5ac55300427dbae9f566
MrCrow13 <ricardo.salgueiro.17@gmail.com>
2020-06-12 17:05:14 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='OpStrings',
+      version='1.0',
+      description='Drawing Distribution Package',
+      author='Your Name',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      packages=['OpStrings'],
+     )
